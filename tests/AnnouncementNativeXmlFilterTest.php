0bf17b91ffe79e3d3a5d8e24d57f9c3bba9ea3b8
Thiago Matos <thiago@lepidus.com.br>
2023-10-09 17:18:46 -0400

    References announcement type in announcement xml
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#645
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- tests/AnnouncementNativeXmlFilterTest.php
+++ tests/AnnouncementNativeXmlFilterTest.php
@@ -7,6 +7,19 @@ import('plugins.importexport.fullJournalTransfer.tests.NativeImportExportFilterT
 
 class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
 {
+    private $announcementExportFilter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $announcementType = $this->createTestAnnouncementType();
+        $this->setUpAnnouncementTypeMockDAO($announcementType);
+
+        $context = $this->createTestContext();
+        $this->announcementExportFilter = $this->getNativeImportExportFilter($context);
+    }
+
     protected function getNativeImportExportFilterClass()
     {
         return AnnouncementNativeXmlFilter::class;
@@ -17,6 +30,24 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
         return 'announcement=>native-xml';
     }
 
+    protected function getMockedDAOs()
+    {
+        return ['AnnouncementTypeDAO'];
+    }
+
+    private function setUpAnnouncementTypeMockDAO($announcementType)
+    {
+        $announcementTypeDAO = $this->getMockBuilder(AnnouncementTypeDAO::class)
+            ->setMethods(['getById'])
+            ->getMock();
+
+        $announcementTypeDAO->expects($this->any())
+            ->method('getById')
+            ->will($this->returnValue($announcementType));
+
+        DAORegistry::registerDAO('AnnouncementTypeDAO', $announcementTypeDAO);
+    }
+
     private function createTestAnnouncement($data)
     {
         $announcement = new Announcement();
@@ -24,10 +55,24 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
         return $announcement;
     }
 
+    private function createTestAnnouncementType()
+    {
+        $announcementType = new AnnouncementType();
+        $announcementType->setName('Test Announcement Type', 'en_US');
+        return $announcementType;
+    }
+
+    private function createTestContext()
+    {
+        $context = Application::getContextDAO()->newDataObject();
+        $context->setId(99);
+        $context->setPrimaryLocale('en_US');
+        return $context;
+    }
+
     public function testAddDateElements()
     {
-        $nativeImportExportFilter = $this->getNativeImportExportFilter();
-        $deployment = $nativeImportExportFilter->getDeployment();
+        $deployment = $this->announcementExportFilter->getDeployment();
 
         $doc = new DOMDocument('1.0');
         $doc->preserveWhiteSpace = false;
@@ -50,7 +95,7 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
             'datePosted' => '2023-01-01 12:00:00.000',
         ]);
         $actualAnnouncementNode = $doc->createElementNS($deployment->getNamespace(), 'announcement');
-        $nativeImportExportFilter->addDates($doc, $actualAnnouncementNode, $announcement);
+        $this->announcementExportFilter->addDates($doc, $actualAnnouncementNode, $announcement);
 
         $this->assertXmlStringEqualsXmlString(
             $doc->saveXML($expectedAnnouncementNode),
@@ -61,8 +106,7 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
 
     public function testCreateAnnouncementNode()
     {
-        $nativeImportExportFilter = $this->getNativeImportExportFilter();
-        $deployment = $nativeImportExportFilter->getDeployment();
+        $deployment = $this->announcementExportFilter->getDeployment();
 
         $doc = new DOMDocument('1.0');
         $doc->preserveWhiteSpace = false;
@@ -73,7 +117,6 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
         $node->setAttribute('type', 'internal');
         $node->setAttribute('advice', 'ignore');
 
-        $expectedAnnouncementNode->setAttribute('type_id', 1);
         $expectedAnnouncementNode->appendChild($node = $doc->createElementNS(
             $deployment->getNamespace(),
             'date_expire',
@@ -84,9 +127,31 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
             'date_posted',
             strftime('%Y-%m-%d %H:%M:%S', strtotime('2023-01-01 12:00:00.000'))
         ));
-        $nativeImportExportFilter->createLocalizedNodes($doc, $expectedAnnouncementNode, 'title', ['en_US' => 'Test Announcement']);
-        $nativeImportExportFilter->createLocalizedNodes($doc, $expectedAnnouncementNode, 'description_short', ['en_US' => '<p>Announcement for test</p>']);
-        $nativeImportExportFilter->createLocalizedNodes($doc, $expectedAnnouncementNode, 'description', ['en_US' => '<p>A announcement created for test purpose</p>']);
+
+        $this->announcementExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedAnnouncementNode,
+            'title',
+            ['en_US' => 'Test Announcement']
+        );
+        $this->announcementExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedAnnouncementNode,
+            'description_short',
+            ['en_US' => '<p>Announcement for test</p>']
+        );
+        $this->announcementExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedAnnouncementNode,
+            'description',
+            ['en_US' => '<p>A announcement created for test purpose</p>']
+        );
+
+        $expectedAnnouncementNode->appendChild($doc->createElementNS(
+            $deployment->getNamespace(),
+            'announcement_type_ref',
+            htmlspecialchars('Test Announcement Type', ENT_COMPAT, 'UTF-8')
+        ));
 
         $announcement = $this->createTestAnnouncement([
             'id' => 21,
@@ -106,7 +171,7 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
             ]
         ]);
 
-        $actualAnnouncementNode = $nativeImportExportFilter->createAnnouncementNode($doc, $announcement);
+        $actualAnnouncementNode = $this->announcementExportFilter->createAnnouncementNode($doc, $announcement);
 
         $this->assertXmlStringEqualsXmlString(
             $doc->saveXML($expectedAnnouncementNode),
@@ -117,7 +182,6 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
 
     public function testCreateCompleteAnnouncementXml()
     {
-        $nativeImportExportFilter = $this->getNativeImportExportFilter();
         $announcements = [$this->createTestAnnouncement([
             'id' => 21,
             'assocId' => 12,
@@ -136,7 +200,7 @@ class AnnouncementNativeXmlFilterTest extends NativeImportExportFilterTestCase
             ]
         ])];
 
-        $doc = $nativeImportExportFilter->process($announcements, true);
+        $doc = $this->announcementExportFilter->process($announcements, true);
 
         $this->assertXmlStringEqualsXmlString(
             $this->getSampleXml('announcement.xml')->saveXml(),
