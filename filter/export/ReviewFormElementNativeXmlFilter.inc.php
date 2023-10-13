22f408c3a5045dba34a58d36856a6e0fc92d9357
Thiago Matos <thiago@lepidus.com.br>
2023-10-13 17:36:14 -0400

    Implements export filter of review form element
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#650
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

---
+++ filter/export/ReviewFormElementNativeXmlFilter.inc.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Copyright (c) 2019-2023 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ */
+
+import('lib.pkp.plugins.importexport.native.filter.NativeExportFilter');
+
+class ReviewFormElementNativeXmlFilter extends NativeExportFilter
+{
+    public function __construct($filterGroup)
+    {
+        $this->setDisplayName('Native XML review form element export');
+        parent::__construct($filterGroup);
+    }
+
+    public function getClassName()
+    {
+        return 'plugins.importexport.native.filter.export.ReviewFormElementNativeXmlFilter';
+    }
+
+    public function &process(&$reviewFormElements)
+    {
+        $doc = new DOMDocument('1.0');
+        $doc->preserveWhiteSpace = false;
+        $doc->formatOutput = true;
+        $deployment = $this->getDeployment();
+
+        $rootNode = $doc->createElementNS($deployment->getNamespace(), 'review_form_elements');
+        foreach ($reviewFormElements as $reviewFormElement) {
+            $rootNode->appendChild($this->createReviewFormElementNode($doc, $reviewFormElement));
+        }
+        $doc->appendChild($rootNode);
+        $rootNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $rootNode->setAttribute('xsi:schemaLocation', $deployment->getNamespace() . ' ' . $deployment->getSchemaFilename());
+
+        return $doc;
+    }
+
+    public function createReviewFormElementNode($doc, $reviewFormElement)
+    {
+        $deployment = $this->getDeployment();
+
+        $reviewFormElementNode = $doc->createElementNS($deployment->getNamespace(), 'review_form_element');
+        $reviewFormElementNode->setAttribute('seq', $reviewFormElement->getSequence());
+        $reviewFormElementNode->setAttribute('element_type', $reviewFormElement->getElementType());
+        $reviewFormElementNode->setAttribute('required', $reviewFormElement->getRequired());
+        $reviewFormElementNode->setAttribute('included', $reviewFormElement->getIncluded());
+
+        $this->createLocalizedNodes(
+            $doc,
+            $reviewFormElementNode,
+            'question',
+            $reviewFormElement->getQuestion(null)
+        );
+        $this->createLocalizedNodes(
+            $doc,
+            $reviewFormElementNode,
+            'description',
+            $reviewFormElement->getDescription(null)
+        );
+        if ($reviewFormElement->getPossibleResponses(null)) {
+            $this->addPossibleResponsesNode(
+                $doc,
+                $reviewFormElementNode,
+                $reviewFormElement->getPossibleResponses(null)
+            );
+        }
+
+        return $reviewFormElementNode;
+    }
+
+    public function addPossibleResponsesNode($doc, $reviewFormElementNode, $possibleResponses)
+    {
+        $deployment = $this->getDeployment();
+
+        foreach ($possibleResponses as $locale => $values) {
+            $reviewFormElementNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'possible_responses'));
+            $node->setAttribute('locale', $locale);
+            foreach ($values as $possibleResponse) {
+                $node->appendChild($childNode = $doc->createElementNS(
+                    $deployment->getNamespace(),
+                    'possible_response',
+                    htmlspecialchars($possibleResponse, ENT_COMPAT, 'UTF-8')
+                ));
+            }
+        }
+    }
+}
---
+++ tests/export/ReviewFormElementNativeXmlFilterTest.php
@@ -0,0 +1,137 @@
+<?php
+
+import('plugins.importexport.fullJournalTransfer.tests.NativeImportExportFilterTestCase');
+import('plugins.importexport.fullJournalTransfer.filter.export.ReviewFormElementNativeXmlFilter');
+
+class ReviewFormElementNativeXmlFilterTest extends NativeImportExportFilterTestCase
+{
+    protected function getSymbolicFilterGroup()
+    {
+        return 'review-form-element=>native-xml';
+    }
+
+    protected function getNativeImportExportFilterClass()
+    {
+        return ReviewFormElementNativeXmlFilter::class;
+    }
+
+    private function createPossibleResponsesNode($doc, $deployment, $locale, $possibleResponses)
+    {
+        $possibleResponsesNode = $doc->createElementNS($deployment->getNamespace(), 'possible_responses');
+        $possibleResponsesNode->setAttribute('locale', $locale);
+        foreach ($possibleResponses as $possibleResponse) {
+            $possibleResponsesNode->appendChild($node = $doc->createElementNS(
+                $deployment->getNamespace(),
+                'possible_response',
+                htmlspecialchars($possibleResponse, ENT_COMPAT, 'UTF-8')
+            ));
+        }
+
+        return $possibleResponsesNode;
+    }
+
+    public function testAddPossibleResponsesNode()
+    {
+        $reviewFormElementExportFilter = $this->getNativeImportExportFilter();
+        $deployment = $reviewFormElementExportFilter->getDeployment();
+
+        $possibleResponses = [
+            'en_US' => ['option 1','option 2', 'option 3']
+        ];
+
+        $doc = new DOMDocument('1.0');
+        $doc->preserveWhiteSpace = false;
+        $doc->formatOutput = true;
+
+        $expectedReviewFormElementNode = $doc->createElementNS($deployment->getNamespace(), 'review_form_element');
+        $possibleResponsesNode = $this->createPossibleResponsesNode(
+            $doc,
+            $deployment,
+            'en_US',
+            $possibleResponses['en_US']
+        );
+        $expectedReviewFormElementNode->appendChild($possibleResponsesNode);
+
+        $actualReviewFormElementNode = $doc->createElementNS($deployment->getNamespace(), 'review_form_element');
+        $reviewFormElementExportFilter->addPossibleResponsesNode(
+            $doc,
+            $actualReviewFormElementNode,
+            $possibleResponses
+        );
+
+        $this->assertXmlStringEqualsXmlString(
+            $doc->saveXML($actualReviewFormElementNode),
+            $doc->saveXML($expectedReviewFormElementNode),
+            "actual xml is equal to expected xml"
+        );
+    }
+
+    public function testCreateReviewFormElementNode()
+    {
+        $reviewFormElementExportFilter = $this->getNativeImportExportFilter();
+        $deployment = $reviewFormElementExportFilter->getDeployment();
+
+        $doc = new DOMDocument('1.0');
+        $doc->preserveWhiteSpace = false;
+        $doc->formatOutput = true;
+
+        $expectedReviewFormElementNode = $doc->createElementNS($deployment->getNamespace(), 'review_form_element');
+        $expectedReviewFormElementNode->setAttribute('seq', 1);
+        $expectedReviewFormElementNode->setAttribute('element_type', 1);
+        $expectedReviewFormElementNode->setAttribute('required', 1);
+        $expectedReviewFormElementNode->setAttribute('included', 1);
+        $reviewFormElementExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedReviewFormElementNode,
+            'question',
+            ['en_US' => '<p>What is your pet name?</p>']
+        );
+        $reviewFormElementExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedReviewFormElementNode,
+            'description',
+            ['en_US' => '<p>A review form element for test purpose</p>']
+        );
+
+        $reviewFormElement = DAORegistry::getDAO('ReviewFormElementDAO')->newDataObject();
+        $reviewFormElement->setSequence(1);
+        $reviewFormElement->setElementType(REVIEW_FORM_ELEMENT_TYPE_SMALL_TEXT_FIELD);
+        $reviewFormElement->setRequired(1);
+        $reviewFormElement->setIncluded(1);
+        $reviewFormElement->setQuestion('<p>What is your pet name?</p>', 'en_US');
+        $reviewFormElement->setDescription('<p>A review form element for test purpose</p>', 'en_US');
+
+        $actualReviewFormElementNode = $reviewFormElementExportFilter->createReviewFormElementNode(
+            $doc,
+            $reviewFormElement
+        );
+
+        $this->assertXmlStringEqualsXmlString(
+            $doc->saveXML($expectedReviewFormElementNode),
+            $doc->saveXML($actualReviewFormElementNode),
+            "actual xml is equal to expected xml"
+        );
+    }
+
+    public function testCreateCompleteReviewFormElementXml()
+    {
+        $reviewFormElementExportFilter = $this->getNativeImportExportFilter();
+
+        $reviewFormElement = DAORegistry::getDAO('ReviewFormElementDAO')->newDataObject();
+        $reviewFormElement->setSequence(1);
+        $reviewFormElement->setElementType(REVIEW_FORM_ELEMENT_TYPE_RADIO_BUTTONS);
+        $reviewFormElement->setRequired(1);
+        $reviewFormElement->setIncluded(1);
+        $reviewFormElement->setQuestion('<p>How are you?</p>', 'en_US');
+        $reviewFormElement->setDescription('<p>A review form element for test purpose</p>', 'en_US');
+        $reviewFormElement->setPossibleResponses(['Fine','Same as always', 'Bad'], 'en_US');
+        $reviewFormElements = [$reviewFormElement];
+
+        $doc = $reviewFormElementExportFilter->process($reviewFormElements);
+        $this->assertXmlStringEqualsXmlString(
+            $this->getSampleXml('reviewFormElement.xml')->saveXml(),
+            $doc->saveXML(),
+            "actual xml is equal to expected xml"
+        );
+    }
+}
