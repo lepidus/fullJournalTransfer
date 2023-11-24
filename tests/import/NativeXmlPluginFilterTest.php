14ec0ae00b9edad3e33beef3d3ccfd9f609b434e
Thiago Matos <thiago@lepidus.com.br>
2023-11-24 12:34:03 -0400

    Imports plugin settings from xml
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#687
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Yves Amorim <yves@lepidus.com.br>

---
+++ tests/import/NativeXmlPluginFilterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+import('plugins.importexport.fullJournalTransfer.tests.NativeImportExportFilterTestCase');
+import('plugins.importexport.fullJournalTransfer.filter.import.NativeXmlPluginFilter');
+
+class NativeXmlPluginFilterTest extends NativeImportExportFilterTestCase
+{
+    protected function getSymbolicFilterGroup()
+    {
+        return 'native-xml=>plugin';
+    }
+
+    protected function getNativeImportExportFilterClass()
+    {
+        return NativeXmlPluginFilter::class;
+    }
+
+    protected function getAffectedTables()
+    {
+        return ['plugin_settings',];
+    }
+
+    public function testHandlePluginElement()
+    {
+        $pluginImportFilter = $this->getNativeImportExportFilter();
+        $deployment = $pluginImportFilter->getDeployment();
+        $pluginSettingsDAO = DAORegistry::getDAO('PluginSettingsDAO');
+
+        $doc = $this->getSampleXml('plugin.xml');
+
+        $expectedPluginSettings = [
+            'enabled' => 1,
+            'someSetting' => 'Test value',
+            'anotherSetting' => 'Another test value'
+        ];
+
+        $pluginNode = $doc->getElementsByTagNameNS($deployment->getNamespace(), 'plugin');
+
+        $pluginSettings = $pluginImportFilter->handleElement($pluginNode->item(0));
+
+        $this->assertEquals($expectedPluginSettings, $pluginSettings);
+        $settings = $pluginSettingsDAO->getPluginSettings(0, 'testPlugin');
+
+        $this->assertEquals($expectedPluginSettings, $settings);
+    }
+}
