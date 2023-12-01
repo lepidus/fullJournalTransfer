e40365f02b509512824fa5c811ac2676c7c03b23
Thiago Matos <thiago@lepidus.com.br>
2023-12-01 18:49:32 -0400

    Creates navigation menu item node
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#699
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Yves Amorim <yves@lepidus.com.br>

---
+++ filter/export/NavigationMenuItemNativeXmlFilter.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright (c) 2019-2023 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ */
+
+import('lib.pkp.plugins.importexport.native.filter.NativeExportFilter');
+
+class NavigationMenuItemNativeXmlFilter extends NativeExportFilter
+{
+    public function __construct($filterGroup)
+    {
+        $this->setDisplayName('Native XML navigation menu item export');
+        parent::__construct($filterGroup);
+    }
+
+    public function getClassName()
+    {
+        return 'plugins.importexport.fullJournalTransfer.filter.export.NavigationMenuItemNativeXmlFilter';
+    }
+
+    public function createNavigationMenuItemNode($doc, $navigationMenuItem)
+    {
+        $deployment = $this->getDeployment();
+
+        $navigationMenuItemNode = $doc->createElementNS($deployment->getNamespace(), 'navigation_menu_item');
+
+        $navigationMenuItemNode->setAttribute('type', $navigationMenuItem->getType());
+        $navigationMenuItemNode->setAttribute('path', $navigationMenuItem->getPath());
+        $navigationMenuItemNode->setAttribute('title_locale_key', $navigationMenuItem->getTitleLocaleKey());
+
+        $this->createLocalizedNodes(
+            $doc,
+            $navigationMenuItemNode,
+            'title',
+            $navigationMenuItem->getTitle(null)
+        );
+        $this->createLocalizedNodes(
+            $doc,
+            $navigationMenuItemNode,
+            'content',
+            $navigationMenuItem->getContent(null)
+        );
+        $this->createLocalizedNodes(
+            $doc,
+            $navigationMenuItemNode,
+            'remote_url',
+            $navigationMenuItem->getRemoteUrl(null)
+        );
+
+        return $navigationMenuItemNode;
+    }
+}
---
+++ tests/export/NavigationMenuItemNativeXmlFilterTest.php
@@ -0,0 +1,67 @@
+<?php
+
+import('lib.pkp.classes.navigationMenu.NavigationMenuItem');
+import('plugins.importexport.fullJournalTransfer.tests.NativeImportExportFilterTestCase');
+import('plugins.importexport.fullJournalTransfer.filter.export.NavigationMenuItemNativeXmlFilter');
+
+class NavigationMenuItemNativeXmlFilterTest extends NativeImportExportFilterTestCase
+{
+    protected function getNativeImportExportFilterClass()
+    {
+        return NavigationMenuItemNativeXmlFilter::class;
+    }
+
+    protected function getSymbolicFilterGroup()
+    {
+        return 'navigation-menu-item=>native-xml';
+    }
+
+    public function testCreateNavigationMenuItemNode()
+    {
+        $navMenuItemExportFilter = $this->getNativeImportExportFilter();
+        $deployment = $navMenuItemExportFilter->getDeployment();
+
+        $doc = new DOMDocument('1.0');
+        $doc->preserveWhiteSpace = false;
+        $doc->formatOutput = true;
+
+        $expectedNavMenuItemNode = $doc->createElementNS($deployment->getNamespace(), 'navigation_menu_item');
+        $expectedNavMenuItemNode->setAttribute('type', NMI_TYPE_ABOUT);
+        $expectedNavMenuItemNode->setAttribute('path', 'testItem');
+        $expectedNavMenuItemNode->setAttribute('title_locale_key', 'navigation.about');
+        $navMenuItemExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedNavMenuItemNode,
+            'title',
+            ['en_US' => 'Test Nav Menu Item Title']
+        );
+        $navMenuItemExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedNavMenuItemNode,
+            'content',
+            ['en_US' => 'Test Nav Menu Item Content']
+        );
+        $navMenuItemExportFilter->createLocalizedNodes(
+            $doc,
+            $expectedNavMenuItemNode,
+            'remote_url',
+            ['en_US' => 'http://path/to/page']
+        );
+
+        $navigationMenuItem = new NavigationMenuItem();
+        $navigationMenuItem->setType(NMI_TYPE_ABOUT);
+        $navigationMenuItem->setPath('testItem');
+        $navigationMenuItem->setTitleLocaleKey('navigation.about');
+        $navigationMenuItem->setTitle('Test Nav Menu Item Title', 'en_US');
+        $navigationMenuItem->setContent('Test Nav Menu Item Content', 'en_US');
+        $navigationMenuItem->setRemoteUrl('http://path/to/page', 'en_US');
+
+        $actualNavMenuItemNode = $navMenuItemExportFilter->createNavigationMenuItemNode($doc, $navigationMenuItem);
+
+        $this->assertXmlStringEqualsXmlString(
+            $doc->saveXML($expectedNavMenuItemNode),
+            $doc->saveXML($actualNavMenuItemNode),
+            "actual xml is equal to expected xml"
+        );
+    }
+}
