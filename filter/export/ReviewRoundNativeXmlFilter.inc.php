b312f174841a10d1983c3788b6aab11056b5738a
Thiago Matos <thiago@lepidus.com.br>
2023-12-18 18:37:16 -0400

    Create the review round node
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#716
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Yves Amorim <yves@lepidus.com.br>

---
+++ filter/export/ReviewRoundNativeXmlFilter.inc.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright (c) 2014-2023 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ */
+
+import('lib.pkp.plugins.importexport.native.filter.NativeExportFilter');
+
+class ReviewRoundNativeXmlFilter extends NativeExportFilter
+{
+    public function __construct($filterGroup)
+    {
+        $this->setDisplayName('Native XML review round export');
+        parent::__construct($filterGroup);
+    }
+
+    public function getClassName()
+    {
+        return 'plugins.importexport.fullJournalTransfer.filter.export.ReviewRoundNativeXmlFilter';
+    }
+
+    public function createReviewRoundNode($doc, $reviewRound)
+    {
+        $deployment = $this->getDeployment();
+
+        $reviewRoundNode = $doc->createElementNS($deployment->getNamespace(), 'review_round');
+
+        $reviewRoundNode->appendChild($node = $doc->createElementNS(
+            $deployment->getNamespace(),
+            'id',
+            $reviewRound->getId()
+        ));
+        $node->setAttribute('type', 'internal');
+        $node->setAttribute('advice', 'ignore');
+
+        $reviewRoundNode->appendChild($doc->createElementNS(
+            $deployment->getNamespace(),
+            'submission_id',
+            intval($reviewRound->getSubmissionId())
+        ));
+        $workflowStageDao = DAORegistry::getDAO('WorkflowStageDAO');
+        $reviewRoundNode->appendChild($doc->createElementNS(
+            $deployment->getNamespace(),
+            'stage',
+            htmlspecialchars(WorkflowStageDAO::getPathFromId($reviewRound->getStageId()), ENT_COMPAT, 'UTF-8')
+        ));
+        $reviewRoundNode->appendChild($doc->createElementNS(
+            $deployment->getNamespace(),
+            'round',
+            intval($reviewRound->getRound())
+        ));
+        $reviewRoundNode->appendChild($doc->createElementNS(
+            $deployment->getNamespace(),
+            'status',
+            intval($reviewRound->getStatus())
+        ));
+
+        return $reviewRoundNode;
+    }
+}
