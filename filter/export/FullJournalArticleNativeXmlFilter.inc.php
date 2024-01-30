ef9b5a72d754cd2ba0659b5cb1e99814830bbe4c
yves <yves@lepidus.com.br>
2024-01-30 17:28:17 -0400

    Extend Article Export Filter to include ReviewRound.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#740
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Yves Amorim <yves@lepidus.com.br>

---
+++ filter/export/FullJournalArticleNativeXmlFilter.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+import('plugins.importexport.native.filter.ArticleNativeXmlFilter');
+
+class FullJournalArticleNativeXmlFilter extends ArticleNativeXmlFilter
+{
+    public function __construct($filterGroup)
+    {
+        parent::__construct($filterGroup);
+    }
+
+    public function getClassName()
+    {
+        return 'plugins.importexport.fullJournalTransfer.filter.export.FullJournalArticleNativeXmlFilter';
+    }
+
+    public function createSubmissionNode($doc, $submission)
+    {
+        $deployment = $this->getDeployment();
+        $submissionNode = parent::createSubmissionNode($doc, $submission);
+
+        $this->addReviewRounds($doc, $submissionNode, $submission);
+        $this->addReviewAssignments($doc, $submissionNode, $submission);
+
+        return $submissionNode;
+    }
+
+    public function addReviewRounds($doc, $submissionNode, $submission)
+    {
+        $filterDao = DAORegistry::getDAO('FilterDAO');
+        $nativeExportFilters = $filterDao->getObjectsByGroup('review-round=>native-xml');
+        assert(count($nativeExportFilters) == 1);
+        $exportFilter = array_shift($nativeExportFilters);
+        $exportFilter->setDeployment($this->getDeployment());
+
+        $reviewRoundDAO = DAORegistry::getDAO('ReviewRoundDAO');
+        $reviewRounds = $reviewRoundDAO->getBySubmissionId($submission->getId())->toArray();
+
+        $reviewRoundsDoc = $exportFilter->execute($reviewRounds);
+        if ($reviewRoundsDoc->documentElement instanceof DOMElement) {
+            $clone = $doc->importNode($reviewRoundsDoc->documentElement, true);
+            $submissionNode->appendChild($clone);
+        }
+    }
+}
