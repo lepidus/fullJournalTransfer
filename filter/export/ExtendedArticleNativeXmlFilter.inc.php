87dcb7f4f6452ecd212851d9c4949aeb43529b6e
Thiago Matos <thiago@lepidus.com.br>
2024-04-01 15:48:41 -0400

    Refactoring article export for workflow stages
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#785
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- filter/export/ExtendedArticleNativeXmlFilter.inc.php
+++ filter/export/ExtendedArticleNativeXmlFilter.inc.php
@@ -17,124 +17,28 @@ class ExtendedArticleNativeXmlFilter extends ArticleNativeXmlFilter
     public function createSubmissionNode($doc, $submission)
     {
         $deployment = $this->getDeployment();
-
         $submissionNode = parent::createSubmissionNode($doc, $submission);
-
-        $this->addStageAssignments($doc, $submissionNode, $submission);
-        $this->addReviewRounds($doc, $submissionNode, $submission);
-        $this->addEditorDecisions($doc, $submissionNode, $submission);
 
         return $submissionNode;
     }
 
-    public function addStageAssignments($doc, $submissionNode, $submission)
+    public function createStageNodes($doc, $submissionNode, $submission)
     {
         $deployment = $this->getDeployment();
-        $context = $deployment->getContext();
-
-        $stageAssignments = DAORegistry::getDAO('StageAssignmentDAO')
-            ->getBySubmissionAndStageId($submission->getId());
-
-        while ($stageAssignment = $stageAssignments->next()) {
-            $validAssignment = $this->validateAssignment(
-                $context->getId(),
-                $stageAssignment->getUserId(),
-                $stageAssignment->getStageId(),
-            );
-            if (!$validAssignment) {
-                continue;
-            }
-
-            $user = DAORegistry::getDAO('UserDAO')
-                ->getById($stageAssignment->getUserId());
-            $userGroup = DAORegistry::getDAO('UserGroupDAO')
-                ->getById($stageAssignment->getUserGroupId(), $context->getId());
-            $stagePath = WorkflowStageDAO::getPathFromId($stageAssignment->getStageId());
-
-            $stageAssignmentNode = $doc->createElementNS($deployment->getNamespace(), 'stage_assignment');
-            $stageAssignmentNode->setAttribute('user', $user->getUsername());
-            $stageAssignmentNode->setAttribute('user_group_ref', $userGroup->getName($context->getPrimaryLocale()));
-            $stageAssignmentNode->setAttribute('stage', $stagePath);
-            $stageAssignmentNode->setAttribute('recommend_only', (int) $stageAssignment->getRecommendOnly());
-            $stageAssignmentNode->setAttribute('can_change_metadata', (int) $stageAssignment->getCanChangeMetadata());
-            $submissionNode->appendChild($stageAssignmentNode);
+        foreach ($this->getStageMapping() as $stageId => $stagePath) {
+            $submissionNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+            $stageNode->setAttribute('path', $stagePath);
         }
     }
 
-    public function addReviewRounds($doc, $submissionNode, $submission)
+    private function getStageMapping()
     {
-        $filterDao = DAORegistry::getDAO('FilterDAO');
-        $nativeExportFilters = $filterDao->getObjectsByGroup('review-round=>native-xml');
-        assert(count($nativeExportFilters) == 1);
-        $exportFilter = array_shift($nativeExportFilters);
-        $exportFilter->setDeployment($this->getDeployment());
-
-        $reviewRoundDAO = DAORegistry::getDAO('ReviewRoundDAO');
-        $reviewRounds = $reviewRoundDAO->getBySubmissionId($submission->getId())->toArray();
-
-        $reviewRoundsDoc = $exportFilter->execute($reviewRounds);
-        if ($reviewRoundsDoc->documentElement instanceof DOMElement) {
-            $clone = $doc->importNode($reviewRoundsDoc->documentElement, true);
-            $submissionNode->appendChild($clone);
-        }
-    }
-
-    public function addEditorDecisions($doc, $submissionNode, $submission)
-    {
-        $deployment = $this->getDeployment();
-
-        $editDecisionDao = DAORegistry::getDAO('EditDecisionDAO');
-        $editorDecisions = $editDecisionDao->getEditorDecisions($submission->getId());
-
-        if (!count($editorDecisions)) {
-            return;
-        }
-
-        $editorDecisionsNode = $doc->createElementNS($deployment->getNamespace(), 'editor_decisions');
-        foreach ($editorDecisions as $editorDecision) {
-            $validAssignment = $this->validateAssignment(
-                $submission->getContextId(),
-                $editorDecision['editorId'],
-                $editorDecision['stageId'],
-            );
-            if (!$validAssignment) {
-                continue;
-            }
-
-            $userDAO = DAORegistry::getDAO('UserDAO');
-            $editor = $userDAO->getById($editorDecision['editorId']);
-
-            $editorDecisionNode = $doc->createElementNS($deployment->getNamespace(), 'editor_decision');
-            $editorDecisionNode->setAttribute('submission_id', $submission->getId());
-            $editorDecisionNode->setAttribute('round', $editorDecision['round']);
-            $editorDecisionNode->setAttribute('review_round_id', $editorDecision['reviewRoundId'] ?: 0);
-            $editorDecisionNode->setAttribute('stage_id', $editorDecision['stageId']);
-            $editorDecisionNode->setAttribute('decision', $editorDecision['decision']);
-
-            $editorDecisionNode->appendChild($node = $doc->createElementNS(
-                $deployment->getNamespace(),
-                'editor',
-                htmlspecialchars($editor->getUsername(), ENT_COMPAT, 'UTF-8')
-            ));
-            $editorDecisionNode->appendChild($node = $doc->createElementNS(
-                $deployment->getNamespace(),
-                'date_decided',
-                strftime('%Y-%m-%d %H:%M:%S', strtotime($editorDecision['dateDecided']))
-            ));
-
-            $editorDecisionsNode->appendChild($editorDecisionNode);
-        }
-
-        $submissionNode->appendChild($editorDecisionsNode);
-    }
-
-    private function validateAssignment($contextId, $userId, $stageId)
-    {
-        $userGroupDAO = DAORegistry::getDAO('UserGroupDAO');
-        if (!$userGroupDAO->userAssignmentExists($contextId, $userId, $stageId)) {
-            return false;
-        }
-
-        return true;
+        return [
+            WORKFLOW_STAGE_ID_SUBMISSION => WORKFLOW_STAGE_PATH_SUBMISSION,
+            WORKFLOW_STAGE_ID_INTERNAL_REVIEW => WORKFLOW_STAGE_PATH_INTERNAL_REVIEW,
+            WORKFLOW_STAGE_ID_EXTERNAL_REVIEW => WORKFLOW_STAGE_PATH_EXTERNAL_REVIEW,
+            WORKFLOW_STAGE_ID_EDITING => WORKFLOW_STAGE_PATH_EDITING,
+            WORKFLOW_STAGE_ID_PRODUCTION => WORKFLOW_STAGE_PATH_PRODUCTION
+        ];
     }
 }
--- tests/export/ExtendedArticleNativeXmlFilterTest.php
+++ tests/export/ExtendedArticleNativeXmlFilterTest.php
@@ -1,7 +1,6 @@
 <?php
 
 import('classes.submission.Submission');
-import('classes.workflow.EditorDecisionActionsManager');
 import('plugins.importexport.fullJournalTransfer.tests.NativeImportExportFilterTestCase');
 import('plugins.importexport.fullJournalTransfer.filter.export.ExtendedArticleNativeXmlFilter');
 
@@ -17,280 +16,31 @@ class ExtendedArticleNativeXmlFilterTest extends NativeImportExportFilterTestCas
         return ExtendedArticleNativeXmlFilter::class;
     }
 
-    protected function getMockedDAOs()
+    public function testCreateStagesNode()
     {
-        return ['UserDAO', 'UserGroupDAO', 'ReviewRoundDAO', 'EditDecisionDAO', 'StageAssignmentDAO'];
-    }
+        $articleExportFilter = $this->getNativeImportExportFilter();
+        $deployment = $articleExportFilter->getDeployment();
 
-    private function registerMockStageAssignmentDAO()
-    {
-        $mockDAO = $this->getMockBuilder(StageAssignmentDAO::class)
-            ->setMethods(['getBySubmissionAndStageId'])
-            ->getMock();
-
-        $stageAssignment = $mockDAO->newDataObject();
-        $stageAssignment->setId(563);
-        $stageAssignment->setStageId(WORKFLOW_STAGE_ID_EXTERNAL_REVIEW);
-        $stageAssignment->getRecommendOnly(0);
-        $stageAssignment->getCanChangeMetadata(0);
-
-        $mockResult = $this->getMockBuilder(DAOResultFactory::class)
-            ->setMethods(['next'])
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $mockResult->expects($this->any())
-            ->method('next')
-            ->will($this->onConsecutiveCalls($stageAssignment, null));
-
-        $mockDAO->expects($this->any())
-            ->method('getBySubmissionAndStageId')
-            ->will($this->returnValue($mockResult));
-
-        DAORegistry::registerDAO('StageAssignmentDAO', $mockDAO);
-    }
-
-    private function registerMockReviewRoundDAO()
-    {
-        $mockDAO = $this->getMockBuilder(ReviewRoundDAO::class)
-            ->setMethods(['getBySubmissionId'])
-            ->getMock();
-
-        $reviewRound = $mockDAO->newDataObject();
-        $reviewRound->setId(563);
-        $reviewRound->setSubmissionId(16);
-        $reviewRound->setStageId(3);
-        $reviewRound->setRound(1);
-        $reviewRound->setStatus(1);
-        $mockResult = $this->getMockBuilder(DAOResultFactory::class)
-            ->setMethods(['toArray'])
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $mockResult->expects($this->any())
-            ->method('toArray')
-            ->will($this->returnValue([$reviewRound]));
-
-        $mockDAO->expects($this->any())
-            ->method('getBySubmissionId')
-            ->will($this->returnValue($mockResult));
-
-        DAORegistry::registerDAO('ReviewRoundDAO', $mockDAO);
-    }
-
-    private function registerMockEditorDecisionDAO()
-    {
-        $mockDAO = $this->getMockBuilder(EditDecisionDAO::class)
-            ->setMethods(['getEditorDecisions'])
-            ->getMock();
-
-        $editorDecision = [
-            'editDecisionId' => 123,
-            'reviewRoundId' => 0,
-            'stageId' => 1,
-            'round' => 0,
-            'editorId' => 784,
-            'decision' => 8,
-            'dateDecided' => '2015-03-04 13:39:11'
-        ];
-
-        $mockDAO->expects($this->any())
-            ->method('getEditorDecisions')
-            ->will($this->returnValue([$editorDecision]));
-
-        DAORegistry::registerDAO('EditDecisionDAO', $mockDAO);
-    }
-
-    private function registerMockUserDAO($username)
-    {
-        $mockDAO = $this->getMockBuilder(UserDAO::class)
-            ->setMethods(['getById'])
-            ->getMock();
-
-        $user = $mockDAO->newDataObject();
-        $user->setUsername($username);
-
-        $mockDAO->expects($this->any())
-            ->method('getById')
-            ->will($this->returnValue($user));
-
-        DAORegistry::registerDAO('UserDAO', $mockDAO);
-    }
-
-    private function registerMockUserGroupDAO()
-    {
-        $mockDAO = $this->getMockBuilder(UserGroupDAO::class)
-            ->setMethods(['getById', 'userAssignmentExists'])
-            ->getMock();
-
-        $userGroup = $mockDAO->newDataObject();
-        $userGroup->setId(734);
-        $userGroup->setName('External Reviewer', 'en_US');
-
-        $mockDAO->expects($this->any())
-            ->method('getById')
-            ->will($this->returnValue($userGroup));
-
-        $mockDAO->expects($this->any())
-            ->method('userAssignmentExists')
-            ->will($this->returnValue(true));
-
-        DAORegistry::registerDAO('UserGroupDAO', $mockDAO);
-    }
-
-    public function createRootNode($doc, $deployment, $tagName)
-    {
-        $rootNode = $doc->createElementNS($deployment->getNamespace(), $tagName);
-        $rootNode->setAttributeNS(
-            'http://www.w3.org/2000/xmlns/',
-            'xmlns:xsi',
-            'http://www.w3.org/2001/XMLSchema-instance'
-        );
-        $rootNode->setAttribute(
-            'xsi:schemaLocation',
-            $deployment->getNamespace() . ' ' . $deployment->getSchemaFilename()
-        );
-
-        return $rootNode;
-    }
-
-    public function testAddStageAssignment()
-    {
-        $extendedArticleExportFilter = $this->getNativeImportExportFilter();
-        $deployment = $extendedArticleExportFilter->getDeployment();
-
-        $context = new Journal();
-        $context->setPrimaryLocale('en_US');
-        $deployment->setContext($context);
-
-        $this->registerMockStageAssignmentDAO();
-        $this->registerMockUserDAO('testuser');
-        $this->registerMockUserGroupDAO();
+        $submission = new Submission();
 
         $doc = new DOMDocument('1.0');
         $doc->preserveWhiteSpace = false;
         $doc->formatOutput = true;
 
-        $expectedSubmissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-        $expectedSubmissionNode->appendChild($stageAssignmentNode = $doc->createElementNS(
-            $deployment->getNamespace(),
-            'stage_assignment'
-        ));
-        $stageAssignmentNode->setAttribute('user', 'testuser');
-        $stageAssignmentNode->setAttribute('user_group_ref', 'External Reviewer');
-        $stageAssignmentNode->setAttribute('stage', 'externalReview');
-        $stageAssignmentNode->setAttribute('recommend_only', 0);
-        $stageAssignmentNode->setAttribute('can_change_metadata', 0);
+        $expectedArticleNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
+        $expectedArticleNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+        $stageNode->setAttribute('path', WORKFLOW_STAGE_PATH_SUBMISSION);
+        $expectedArticleNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+        $stageNode->setAttribute('path', WORKFLOW_STAGE_PATH_INTERNAL_REVIEW);
+        $expectedArticleNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+        $stageNode->setAttribute('path', WORKFLOW_STAGE_PATH_EXTERNAL_REVIEW);
+        $expectedArticleNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+        $stageNode->setAttribute('path', WORKFLOW_STAGE_PATH_EDITING);
+        $expectedArticleNode->appendChild($stageNode = $doc->createElementNS($deployment->getNamespace(), 'stage'));
+        $stageNode->setAttribute('path', WORKFLOW_STAGE_PATH_PRODUCTION);
 
-        $submissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-
-        $submission = new Submission();
-        $submission->setId(143);
-
-        $extendedArticleExportFilter->addStageAssignments($doc, $submissionNode, $submission);
-
-        $this->assertXmlStringEqualsXmlString(
-            $doc->saveXML($expectedSubmissionNode),
-            $doc->saveXML($submissionNode),
-            "actual xml is equal to expected xml"
-        );
-    }
-
-    public function createEditorDecisionsNode($doc, $deployment)
-    {
-        $editorDecisionsNode = $doc->createElementNS($deployment->getNamespace(), 'editor_decisions');
-        $editorDecisionsNode->appendChild($editorDecisionNode = $doc->createElementNS(
-            $deployment->getNamespace(),
-            'editor_decision'
-        ));
-
-        $editorDecisionNode->setAttribute('submission_id', 143);
-        $editorDecisionNode->setAttribute('round', 0);
-        $editorDecisionNode->setAttribute('review_round_id', 0);
-        $editorDecisionNode->setAttribute('stage_id', 1);
-        $editorDecisionNode->setAttribute('decision', SUBMISSION_EDITOR_DECISION_EXTERNAL_REVIEW);
-
-        $editorDecisionNode->appendChild($node = $doc->createElementNS(
-            $deployment->getNamespace(),
-            'editor',
-            htmlspecialchars('journaleditor', ENT_COMPAT, 'UTF-8')
-        ));
-        $editorDecisionNode->appendChild($node = $doc->createElementNS(
-            $deployment->getNamespace(),
-            'date_decided',
-            strftime('%Y-%m-%d %H:%M:%S', strtotime('2015-03-04 13:39:11'))
-        ));
-
-        return $editorDecisionsNode;
-    }
-
-    public function testAddReviewRounds()
-    {
-        $extendedArticleExportFilter = $this->getNativeImportExportFilter();
-        $deployment = $extendedArticleExportFilter->getDeployment();
-
-        $reviewRoundNativeXmlFilterTest = new ReviewRoundNativeXmlFilterTest();
-
-        $doc = new DOMDocument('1.0');
-        $doc->preserveWhiteSpace = false;
-        $doc->formatOutput = true;
-
-        $expectedSubmissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-
-        $expectedSubmissionNode->appendChild($reviewRoundsNode = $this->createRootNode(
-            $doc,
-            $deployment,
-            'review_rounds'
-        ));
-        $reviewRoundsNode->appendChild(
-            $reviewRoundNode = $reviewRoundNativeXmlFilterTest->createReviewRoundNode($doc, $deployment)
-        );
-        $reviewRoundNode->appendChild($this->createRootNode($doc, $deployment, 'review_assignments'));
-
-        $actualSubmissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-        $submission = new Submission();
-        $submission->setId(143);
-        $this->registerMockReviewRoundDAO();
-
-        $extendedArticleExportFilter->addReviewRounds($doc, $actualSubmissionNode, $submission);
-
-        $this->assertXmlStringEqualsXmlString(
-            $doc->saveXML($expectedSubmissionNode),
-            $doc->saveXML($actualSubmissionNode),
-            "actual xml is equal to expected xml"
-        );
-    }
-
-    public function testAddEditorDecisions()
-    {
-        $extendedArticleExportFilter = $this->getNativeImportExportFilter();
-        $deployment = $extendedArticleExportFilter->getDeployment();
-
-        $doc = new DOMDocument('1.0');
-        $doc->preserveWhiteSpace = false;
-        $doc->formatOutput = true;
-
-        $submission = new Submission();
-        $submission->setId(143);
-
-        $this->registerMockEditorDecisionDAO();
-        $this->registerMockUserDAO('journaleditor');
-        $this->registerMockUserGroupDAO();
-
-        $expectedSubmissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-        $expectedSubmissionNode->appendChild($this->createEditorDecisionsNode($doc, $deployment));
-
-        $actualSubmissionNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
-        $extendedArticleExportFilter->addEditorDecisions(
-            $doc,
-            $actualSubmissionNode,
-            $submission
-        );
-
-        $this->assertXmlStringEqualsXmlString(
-            $doc->saveXML($expectedSubmissionNode),
-            $doc->saveXML($actualSubmissionNode),
-            "actual xml is equal to expected xml"
-        );
+        $articleNode = $doc->createElementNS($deployment->getNamespace(), 'extended_article');
+        $articleExportFilter->createStageNodes($doc, $articleNode, $submission);
+        $this->assertEquals($expectedArticleNode, $articleNode);
     }
 }
