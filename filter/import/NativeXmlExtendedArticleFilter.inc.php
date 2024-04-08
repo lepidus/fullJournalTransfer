ca6387767ec2c5aac38d8ba96755a1aaa57dbd12
Thiago Matos <thiago@lepidus.com.br>
2024-04-08 12:48:54 -0400

    Parse review assignments in article import
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#785
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- filter/import/NativeXmlExtendedArticleFilter.inc.php
+++ filter/import/NativeXmlExtendedArticleFilter.inc.php
@@ -29,4 +29,43 @@ class NativeXmlExtendedArticleFilter extends NativeXmlArticleFilter
 
         $reviewFormResponseDAO->insertObject($reviewFormResponse);
     }
+
+    public function parseReviewAssignment($node, $reviewRound)
+    {
+        $deployment = $this->getDeployment();
+
+        $reviewAssignmentDAO = DAORegistry::getDAO('ReviewAssignmentDAO');
+        $reviewAssignment = $reviewAssignmentDAO->newDataObject();
+
+        $userDAO = DAORegistry::getDAO('UserDAO');
+        $reviewer = $userDAO->getByUsername($node->getAttribute('reviewer'));
+
+        $reviewAssignment->setSubmissionId($reviewRound->getSubmissionId());
+        $reviewAssignment->setReviewerId($reviewer->getId());
+        $reviewAssignment->setReviewFormId($node->getAttribute('review_form_id'));
+        $reviewAssignment->setReviewRoundId($reviewRound->getId());
+        $reviewAssignment->setCompetingInterests($node->getAttribute('competing_interests'));
+        $reviewAssignment->setRecommendation($node->getAttribute('recommendation'));
+        $reviewAssignment->setDateAssigned($node->getAttribute('date_assigned'));
+        $reviewAssignment->setDateNotified($node->getAttribute('date_notified'));
+        $reviewAssignment->setDateConfirmed($node->getAttribute('date_confirmed'));
+        $reviewAssignment->setDateCompleted($node->getAttribute('date_completed'));
+        $reviewAssignment->setDateAcknowledged($node->getAttribute('date_acknowledged'));
+        $reviewAssignment->setDateDue($node->getAttribute('date_due'));
+        $reviewAssignment->setDateResponseDue($node->getAttribute('date_response_due'));
+        $reviewAssignment->setLastModified($node->getAttribute('last_modified'));
+        $reviewAssignment->setDeclined((int) $node->getAttribute('declined'));
+        $reviewAssignment->setCancelled((int) $node->getAttribute('cancelled'));
+        $reviewAssignment->setQuality($node->getAttribute('quality'));
+        $reviewAssignment->setDateRated($node->getAttribute('date_rated'));
+        $reviewAssignment->setDateReminded($node->getAttribute('date_reminded'));
+        $reviewAssignment->setReminderWasAutomatic((int) $node->getAttribute('reminder_was_automatic'));
+        $reviewAssignment->setRound($reviewRound->getRound());
+        $reviewAssignment->setReviewMethod((int) $node->getAttribute('method'));
+        $reviewAssignment->setStageId($reviewRound->getStageId());
+        $reviewAssignment->setUnconsidered((int) $node->getAttribute('unconsidered'));
+
+        $reviewAssignmentDAO->insertObject($reviewAssignment);
+        return $reviewAssignment;
+    }
 }
--- tests/import/NativeXmlExtendedArticleFilterTest.php
+++ tests/import/NativeXmlExtendedArticleFilterTest.php
@@ -17,7 +17,12 @@ class NativeXmlExtendedArticleFilterTest extends NativeImportExportFilterTestCas
 
     protected function getAffectedTables()
     {
-        return ['review_form_responses'];
+        return ['review_form_responses', 'review_assignments'];
+    }
+
+    protected function getMockedDAOs()
+    {
+        return ['UserDAO', 'ReviewAssignmentDAO'];
     }
 
     public function testParseResponses()
@@ -34,7 +39,7 @@ class NativeXmlExtendedArticleFilterTest extends NativeImportExportFilterTestCas
         $reviewAssignment = new ReviewAssignment();
         $reviewAssignment->setId(81);
 
-        for ($i = 0 ; $i < $responseNodeList->length ; $i++) {
+        for ($i = 0; $i < $responseNodeList->length; $i++) {
             $responseNode = $responseNodeList->item($i);
             $articleImportFilter->parseResponse($responseNode, $reviewAssignment);
         }
@@ -49,5 +54,84 @@ class NativeXmlExtendedArticleFilterTest extends NativeImportExportFilterTestCas
         ];
 
         $this->assertEquals($expectedResponses, $reviewFormResponses);
+    }
+
+    public function testParseReviewAssignment()
+    {
+        $articleImportFilter = $this->getNativeImportExportFilter();
+        $deployment = $articleImportFilter->getDeployment();
+        $doc = $this->getSampleXml('article.xml');
+
+        $reviewRound = new ReviewRound();
+        $reviewRound->setId(23);
+        $reviewRound->setRound(1);
+        $reviewRound->setSubmissionId(75);
+        $reviewRound->setStageId(WORKFLOW_STAGE_ID_EXTERNAL_REVIEW);
+
+        $mockUserDAO = $this->getMockBuilder(UserDAO::class)
+            ->setMethods(['getById', 'getByUsername'])
+            ->getMock();
+
+        $reviewer = $mockUserDAO->newDataObject();
+        $reviewer->setId(52);
+        $reviewer->setUsername('reviewer');
+        $reviewer->setGivenName('reviewer', 'en_US');
+
+        $mockUserDAO->expects($this->any())
+            ->method('getById')
+            ->will($this->returnValue($reviewer));
+
+        $mockUserDAO->expects($this->any())
+            ->method('getByUsername')
+            ->will($this->returnValue($reviewer));
+
+        DAORegistry::registerDAO('UserDAO', $mockUserDAO);
+
+        $mockReviewAssignmentDAO = $this->getMockBuilder(ReviewAssignmentDAO::class)
+            ->setMethods(['newDataObject'])
+            ->getMock();
+
+        $mockReviewAssignmentDAO->expects($this->any())
+            ->method('newDataObject')
+            ->will($this->returnValue(new ReviewAssignment()));
+
+        DAORegistry::registerDAO('ReviewAssignmentDAO', $mockReviewAssignmentDAO);
+
+        $reviewAssignmentList = $doc->getElementsByTagNameNS($deployment->getNamespace(), 'review_assignment');
+        $articleImportFilter->parseReviewAssignment($reviewAssignmentList->item(0), $reviewRound);
+
+        $reviewAssignmentDAO = DAORegistry::getDAO('ReviewAssignmentDAO');
+        $reviewAssignments = $reviewAssignmentDAO->getByReviewRoundId($reviewRound->getId());
+        $reviewAssignment = array_shift($reviewAssignments);
+        unset($reviewAssignment->_data['id']);
+
+        $expectedReviewAssignment = new ReviewAssignment();
+        $expectedReviewAssignment->setReviewerId($reviewer->getId());
+        $expectedReviewAssignment->setSubmissionId($reviewRound->getSubmissionId());
+        $expectedReviewAssignment->setReviewFormId(35);
+        $expectedReviewAssignment->setReviewRoundId($reviewRound->getId());
+        $expectedReviewAssignment->setReviewerFullName($reviewer->getFullName());
+        $expectedReviewAssignment->setRound($reviewRound->getRound());
+        $expectedReviewAssignment->setStageId($reviewRound->getStageId());
+        $expectedReviewAssignment->setRecommendation(SUBMISSION_REVIEWER_RECOMMENDATION_ACCEPT);
+        $expectedReviewAssignment->setQuality(SUBMISSION_REVIEWER_RATING_VERY_GOOD);
+        $expectedReviewAssignment->setReviewMethod(SUBMISSION_REVIEW_METHOD_OPEN);
+        $expectedReviewAssignment->setCompetingInterests('There is no competing interest');
+        $expectedReviewAssignment->setDeclined(0);
+        $expectedReviewAssignment->setCancelled(0);
+        $expectedReviewAssignment->setReminderWasAutomatic(0);
+        $expectedReviewAssignment->setUnconsidered(REVIEW_ASSIGNMENT_NOT_UNCONSIDERED);
+        $expectedReviewAssignment->setDateRated('2023-10-31 21:52:08');
+        $expectedReviewAssignment->setDateReminded('2023-10-30 21:52:08');
+        $expectedReviewAssignment->setDateAssigned('2023-10-29 21:52:08');
+        $expectedReviewAssignment->setDateNotified('2023-10-28 21:52:08');
+        $expectedReviewAssignment->setDateConfirmed('2023-10-27 21:52:08');
+        $expectedReviewAssignment->setDateCompleted('2023-10-26 21:52:08');
+        $expectedReviewAssignment->setDateAcknowledged('2023-10-25 21:52:08');
+        $expectedReviewAssignment->setDateDue('2023-10-24 21:52:08');
+        $expectedReviewAssignment->setDateResponseDue('2023-10-23 21:52:08');
+        $expectedReviewAssignment->setLastModified('2023-10-22 21:52:08');
+
+        $this->assertEquals($expectedReviewAssignment->_data, $reviewAssignment->_data);
     }
 }
